ec54dcc00e5d48938b5b2d9cd1e79e2b51a730eb
tuyishimehono <tuhonori1@gmail.com>
2025-03-27 10:32:21 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WhatsappController } from './whatsapp/whatsapp/whatsapp.controller';
+import { ConfigModule } from '@nestjs/config';
+import { WhatsappModule } from './whatsapp/whatsapp.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), WhatsappModule],
+  controllers: [AppController, WhatsappController],
+  providers: [AppService],
+})
+export class AppModule {}
