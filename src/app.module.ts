6a954de74c4bd61744dea7976a3f15ed29949388
tuyishimehono <tuhonori1@gmail.com>
2025-03-27 11:14:24 +0200

    Move business logic to service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { WhatsappController } from './whatsapp/whatsapp/whatsapp.controller';
 import { ConfigModule } from '@nestjs/config';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { WhatsappService } from './whatsapp/whatsapp/whatsapp.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), WhatsappModule],
   controllers: [AppController, WhatsappController],
-  providers: [AppService],
+  providers: [AppService, WhatsappService],
 })
 export class AppModule {}
--- src/whatsapp/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp/whatsapp.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, BadRequestException, Logger } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { catchError, map } from 'rxjs';
 @Injectable()
-export class WhatsappService {}
+export class WhatsappService {
+  private readonly httpService = new HttpService();
+  private readonly logger = new Logger(WhatsappService.name);
+  async sendWhatsAppMessage(messageSender: string) {
+    const url = `https://graph.facebook.com/${process.env.WHATSAPP_CLOUD_API_VERSION}/${process.env.WHATSAPP_CLOUD_API_PHONE_NUMBER_ID}/messages`;
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.WHATSAPP_CLOUD_API_ACCESS_TOKEN}`,
+      },
+    };
+
+    const data = JSON.stringify({
+      messaging_product: 'whatsapp',
+      recipient_type: 'individual',
+      to: messageSender,
+      type: 'text',
+      text: {
+        preview_url: false,
+        body: 'Response Message from Our Leow Bot!!!',
+      },
+    });
+
+    try {
+      const response = this.httpService
+        .post(url, data, config)
+        .pipe(
+          map((res) => {
+            return res.data;
+          }),
+        )
+        .pipe(
+          catchError((error) => {
+            this.logger.error(error);
+            throw new BadRequestException(
+              'Error Posting to Whatsapp Cloud API',
+            );
+          }),
+        );
+    } catch (error) {
+      this.logger.error(error);
+      return 'Axle broke. Abort Mission';
+    }
+  }
+}
