ee0399ebdda41256c3b2b5fa6b4a912274c7b9cb
tuyishimehono <tuhonori1@gmail.com>
2025-04-09 19:46:29 +0200

    Add questions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { WhatsappController } from './whatsapp/whatsapp.controller';
 import { ConfigModule } from '@nestjs/config';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
 import { WhatsappService } from './whatsapp/whatsapp.service';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WhatsappModule],
+  imports: [ConfigModule.forRoot(), WhatsappModule, QuestionsModule],
   controllers: [AppController, WhatsappController],
   providers: [AppService, WhatsappService],
 })
