7ecbedb258a397cd6920004e174eb7c3a19b254d
tuyishimehono <tuhonori1@gmail.com>
2025-04-15 00:51:32 +0200

    refactor: reorder imports in QuestionsModule for clarity

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { QuestionsService } from './questions.service';
+import { MessageModule } from '../message/message.module';
 
 @Module({
+  imports: [MessageModule],
   controllers: [QuestionsController],
   providers: [QuestionsService],
   exports: [QuestionsService],
