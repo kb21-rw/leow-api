8717019de617d77b2323d17b7ae5c72d9def24bd
tuyishimehono <tuhonori1@gmail.com>
2025-04-14 20:58:55 +0200

    refactor: rename question methods and remove file reading.

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { Question } from './interfaces/question.interface';
-import * as fs from 'fs';
-import * as path from 'path';
+import questions from '../data/questions.json';
 
 @Injectable()
 export class QuestionsService {
-  private questions: Question[];
+  private list: Question[];
 
   constructor() {
-    const jsonPath = path.join(__dirname, '../../src/data/questions.json');
-    const jsonData = fs.readFileSync(jsonPath, 'utf8');
-    this.questions = JSON.parse(jsonData) as Question[];
+    this.list = questions as Question[];
   }
 
-  getAllQuestions(): Question[] {
-    return this.questions;
+  findAll(): Question[] {
+    return this.list;
   }
 
-  getQuestionById(id: number): Question {
-    const question = this.questions.find((question) => question._id === id);
+  findById(id: number): Question {
+    const question = this.list.find((question) => question._id === id);
     if (!question) {
       throw new Error(`Question with ID ${id} not found`);
     }
