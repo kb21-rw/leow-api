6cd1fa2fadf994e925f3d72170804cf7323b9f8d
tuyishimehono <tuhonori1@gmail.com>
2025-04-15 00:52:21 +0200

    refactor: move question related logic to question service

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Question } from './interfaces/question.interface';
 import questions from '../data/questions.json';
+import { MessageService } from '../message/message.service';
+
+interface UserSession {
+  currentQuestionId: number;
+}
 
 @Injectable()
 export class QuestionsService {
+  private readonly logger = new Logger(QuestionsService.name);
   private list: Question[];
+  private userSession = new Map<string, UserSession>();
 
-  constructor() {
+  constructor(private readonly messageService: MessageService) {
     this.list = questions as Question[];
   }
 
@@ -20,5 +27,31 @@ export class QuestionsService {
       throw new Error(`Question with ID ${id} not found`);
     }
     return question;
+  }
+
+  getCurrentQuestionId(messageSender: string): number {
+    if (!this.userSession.has(messageSender)) {
+      this.userSession.set(messageSender, { currentQuestionId: 1 });
+    }
+    return this.userSession.get(messageSender)!.currentQuestionId;
+  }
+
+  async getNext(messageSender: string): Promise<void> {
+    const userSession = this.userSession.get(messageSender)!;
+
+    const nextQuestion = this.findById(userSession.currentQuestionId);
+
+    // Send question with options
+    if (nextQuestion.options && nextQuestion.options.length > 0) {
+      await this.messageService.sendWithOptions(
+        messageSender,
+        nextQuestion.question,
+        nextQuestion.options,
+      );
+
+      userSession.currentQuestionId++;
+    } else {
+      //Todo: Send other question types
+    }
   }
 }
