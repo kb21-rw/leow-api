62338444fa928fb4bdbbd38e97c8402bfe8f4674
tuyishimehono <tuhonori1@gmail.com>
2025-04-16 18:12:45 +0200

    feat: implement user session management with question tracking

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private userSessions = new Map<
+    string,
+    {
+      currentQuestionId: number;
+      completed: boolean;
+    }
+  >();
+
+  getUserSession(messageSender: string) {
+    if (!this.userSessions.has(messageSender)) {
+      this.userSessions.set(messageSender, {
+        currentQuestionId: 1,
+        completed: false,
+      });
+    }
+    return this.userSessions.get(messageSender);
+  }
+
+  incrementCurrentQuestion(messageSender: string) {
+    const userSession = this.getUserSession(messageSender);
+    if (userSession) {
+      userSession.currentQuestionId++;
+    }
+  }
+
+  hasCompletedAllQuestions(
+    currentQuestionId: number,
+    totalQuestions: number,
+  ): boolean {
+    return currentQuestionId > totalQuestions;
+  }
+}
